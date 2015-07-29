9000e4b96a469bceea0bceb7e97c6c01a0eca35e
Furukazu <yuki.ohiro@gmail.com>
2015-07-29 17:22:01 +0900

    ある程度

--- Assets/PathFinding.cs
+++ Assets/PathFinding.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class PathFinding<T>  {
+// http://qiita.com/2dgames_jp/items/f29e915357c1decbc4b7
+
+/// <summary>
+/// A-Starアルゴリズムのそれなりに柔軟な実装
+/// 位置をTで表す前提で、ふたつのT間の仮説距離が計算可能である必要がある
+/// Path finding.
+/// </summary>
+public class PathFinding<T> where T : IHeuristicDistance<T>  {
 
 	public enum PFState{
 		None,
@@ -9,65 +16,200 @@ public class PathFinding<T>  {
 		Closed
 	}
 
+	/// <summary>
+	/// 探索状態の保存域
+	/// The cache.
+	/// </summary>
 	protected Dictionary<T,PFState> cache;
+
+	/// <summary>
+	/// その場所がどこから繋がって探索されたものなのかの保存域。これをたどることによってゴールからスタートに向かって経路が一意に決まる
+	/// The parent.
+	/// </summary>
 	protected Dictionary<T,T> parent;
 
+	/// <summary>
+	/// スタート地点を0として、そこからここまでの総距離の保存域。
+	/// The moved cost.
+	/// </summary>
+	protected Dictionary<T,float> MovedCost;
 
+	/// <summary>
+	/// 次の検索対象となるべき位置の保存域。こいつが常にソートされて先頭から次の探索候補となる
+	/// The next.
+	/// </summary>
 	protected List<T> next;
 
 	protected T Goal;
 	protected T Start;
 
-	protected float GetMoveCost(T pos){
-		return 0.0f;
-	}
 
-	protected float CalcHScore(T Pos){
-		return 0;
+	private float CalcHScore(T Pos){
+		return Goal.CalcHeuristicDistance(Pos);
 	}
 
 	public PathFinding(){
 		cache = new Dictionary<T, PFState>();
 		parent = new Dictionary<T, T>();
 		next = new List<T>();
+		MovedCost = new Dictionary<T, float> ();
 	}
 
-	protected T [] calcNexts(T orig){
+
+	public void SetStartAndGoal(T s,T g){
+		Start = s;
+		Goal = g;
+		OnPostSetup ();
+	}
+
+	protected virtual void OnPostSetup(){
+	}
+
+	/// <summary>
+	/// 引数の地点から、次の探索候補として追加されるべき地点の一覧を返却する。こいつを良い感じにoverrideすることで、地点同士の繋がりを実現する
+	/// Calculates the nexts.
+	/// </summary>
+	/// <returns>The nexts.</returns>
+	/// <param name="orig">Original.</param>
+	protected virtual List<T> calcNexts(T orig){
 		return null;
 	}
 
-	protected void openNext(T pos){
+	/// <summary>
+	/// 引数の地点から次の探索候補を得てそれを追加する。ついでにそこへのスタート地点からの総距離や、繋がりをセットする。
+	/// 地点へのリンク数とは別に距離の重さを設定したい場合はここら辺をoverrideしたりする想定
+	/// Opens the next.
+	/// </summary>
+	/// <param name="pos">Position.</param>
+	protected virtual void openNext(T pos){
 		cache[pos] = PFState.Closed;
+		var bas = MovedCost [pos];
 
 		foreach(var nex in calcNexts(pos)){
+			MovedCost[nex] = bas + 1;
 			parent[nex] = pos;
 			cache[nex] = PFState.Opened;
-			next.Add(pos);
+			next.Add(nex);
 		}
 	}
 
-	public void FindPath(){
-		var c = Start;
+	/// <summary>
+	/// 探索を行った結果を、GoalからStartへの地点のListとして返却する
+	/// Gets the path.
+	/// </summary>
+	/// <returns>The path.</returns>
+	public List<T> GetPath(){
+		var safe = 100;
+		var ret = new List<T>();
 
+		var p = Goal;
+
+	loop:
+			--safe;
+
+		if (safe < 0) {
+			return ret;
+		}
+		ret.Add (p);
+		if (p.Equals (Start)) {
+			return ret;
+		}
+		p = parent [p];
+		goto loop;
+
+	}
+
+	/// <summary>
+	/// 探索を行う。
+	/// Finds the path.
+	/// </summary>
+	public void FindPath(){
+
+		var safe = 100;
+
+		// 初期地点の初期設定。総移動量は0で自分を親としOpened状態にして探索候補一覧にぶっ込んでおく
+		var c = Start;
+		MovedCost [c] = 0;
+		parent [c] = c;
 		cache[c] = PFState.Opened;
 		next.Add(c);
 
-		next.Sort(
-			(t1,t2) =>{
-			if(cache[t1] == PFState.Closed){ return -1;}
-			if(cache[t2] == PFState.Closed){ return 1;}
-			return 
-				System.Math.Sign((GetMoveCost(t1)+CalcHScore(t1))-
-					(GetMoveCost(t2)+CalcHScore(t2)));
-		}
-			);
-		var n = next[0];
-
-		if(n .Equals( Goal)){
+	loop:
+			--safe;
+		if (safe < 0) {
 			return;
 		}
 
+		// 現在の候補一覧の中で、総移動量＋仮定距離の順にソートする。ただしすでに移動したところは無限に遠方とする。
+		next.Sort(
+			(t1,t2) =>{
+			if(cache[t1] == PFState.Closed){ return 1;}
+			if(cache[t2] == PFState.Closed){ return -1;}
+
+			var val1 = (MovedCost[t1]+CalcHScore(t1));
+			var val2 = (MovedCost[t2]+CalcHScore(t2));
+			if(val1==val2){
+				return System.Math.Sign(MovedCost[t1]-MovedCost[t2]);
+			}
+
+			return System.Math.Sign(val1-val2);
+		}
+			);
+
+		// もし候補が存在しなければ打ち切る
+		if (next.Count == 0) {
+			return;
+		}
+
+		// ソートした結果の先頭を候補とする
+		var n = next[0];
+
+		// もし探索済みであれば候補がすべて探索完了したということなので打ち切る
+		if (cache [n] == PFState.Closed) {
+			return;
+		}
+
+		// ゴールと同位置にたどり着いたなら終了
+		if(n.Equals(Goal)){
+			return;
+		}
+
+		// 次の候補はゴールではなかったので、さらにそこから次の候補を一覧へと追加する
 		openNext(n);
+		goto loop;
+	}
+}
+
+public interface IHeuristicDistance<T> {
+	float CalcHeuristicDistance (T dst);
+}
+
+public class Tuple<XType,YType> 
+	where XType : struct 
+	where YType : struct {
+
+	public XType X;
+	public YType Y;
+	
+		public static Tuple<X,Y> Create<X,Y>(X x,Y y) where X : struct where Y : struct {
+			var ret = new Tuple<X, Y> ();
+			ret.X = x;
+			ret.Y = y;
+			return ret;
+		}
+
+	public override bool Equals (object obj)
+	{
+		if (!(obj is Tuple<XType,YType>)) {
+			return false;
+		}
+		var o = (Tuple<XType,YType>)obj;
+		return X.Equals (o.X) && Y.Equals (o.Y);
+	}
+
+	public override int GetHashCode ()
+	{
+		return X.GetHashCode() ^ (Y.GetHashCode() << 1);
 	}
 }
 
---
+++ Assets/Test.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Test : MonoBehaviour {
+
+	// テスト用のフィールドの位置情報を表す
+	private class PlainField : Tuple<int,int>
+		, IHeuristicDistance<PlainField>
+	{
+
+		#region IHeuristicDistance implementation
+		/// <summary>
+		/// 仮定距離は単なるX距離＋Y距離
+		/// Calculates the heuristic distance.
+		/// </summary>
+		/// <returns>The heuristic distance.</returns>
+		/// <param name="dst">Dst.</param>
+		public float CalcHeuristicDistance (PlainField dst)
+		{
+			return System.Math.Abs (X - dst.X) + System.Math.Abs (Y - dst.Y);
+		}
+		#endregion
+
+		public static PlainField Create(int x,int y){
+			var ret = new PlainField ();
+			ret.X = x;
+			ret.Y = y;
+			return ret;
+		}
+
+		public PlainField CreateNext(int x,int y){
+			var ret = new PlainField ();
+			ret.X = X + x;
+			ret.Y = Y + y;
+			return ret;
+		}
+
+		/// <summary>
+		/// さくっと表示させたいので
+		/// Returns a <see cref="System.String"/> that represents the current <see cref="Test+PlainField"/>.
+		/// </summary>
+		/// <returns>A <see cref="System.String"/> that represents the current <see cref="Test+PlainField"/>.</returns>
+		public override string ToString ()
+		{
+			return string.Format ("({0},{1})",X,Y);
+		}
+	}
+
+	// テスト用の経路探索
+	private class MyPathFind : PathFinding<PlainField>{
+
+		/// <summary>
+		/// 隣り合うフィールドは上下左右とします
+		/// 引数の地点から、次の探索候補として追加されるべき地点の一覧を返却する。こいつを良い感じにoverrideすることで、地点同士の繋がりを実現する
+		/// Calculates the nexts.
+		/// </summary>
+		/// <returns>The nexts.</returns>
+		/// <param name="orig">Original.</param>
+		protected override List<PlainField> calcNexts (PlainField orig)
+		{
+			var ret = new List<PlainField> ();
+
+			var xs = new int[]{1,0,-1,0};
+			var ys = new int[]{0,1,0,-1};
+
+			for (var i =0; i<xs.Length; ++i) {
+				var next = orig.CreateNext (xs [i], ys [i]);
+
+				// フィールドのX,Yは0以上9以下とします
+				if(next.X<0 || next.Y<0 || next.X>9 || next.Y>9){ continue;}
+				if (!cache.ContainsKey (next)) {
+					//cache [next] = PFState.Opened;
+					ret.Add (next);
+				}
+			}
+
+			return ret;
+		}
+
+		protected override void OnPostSetup ()
+		{
+			base.OnPostSetup ();
+
+			// 障害物があるとする
+			cache [PlainField.Create (0,3)] = PFState.Closed;
+			cache [PlainField.Create (1,3)] = PFState.Closed;
+			cache [PlainField.Create (2,3)] = PFState.Closed;
+			cache [PlainField.Create (3,3)] = PFState.Closed;
+			cache [PlainField.Create (4,3)] = PFState.Closed;
+			cache [PlainField.Create (5,3)] = PFState.Closed;
+			cache [PlainField.Create (6,3)] = PFState.Closed;
+			cache [PlainField.Create (7,3)] = PFState.Closed;
+			cache [PlainField.Create (8,3)] = PFState.Closed;
+
+			cache [PlainField.Create (9,6)] = PFState.Closed;
+			cache [PlainField.Create (8,6)] = PFState.Closed;
+			cache [PlainField.Create (7,6)] = PFState.Closed;
+			cache [PlainField.Create (6,6)] = PFState.Closed;
+			cache [PlainField.Create (5,6)] = PFState.Closed;
+			cache [PlainField.Create (4,6)] = PFState.Closed;
+			cache [PlainField.Create (3,6)] = PFState.Closed;
+			cache [PlainField.Create (2,6)] = PFState.Closed;
+			cache [PlainField.Create (1,6)] = PFState.Closed;
+
+		}
+	}
+
+	private MyPathFind myp;
+
+	// Use this for initialization
+	void Start () {
+		myp = new MyPathFind ();
+		// 0,0から9,9までの経路探索
+		myp.SetStartAndGoal (PlainField.Create (0, 0), PlainField.Create (9, 9));
+		myp.FindPath ();
+		var p = myp.GetPath ();
+		print (p);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
